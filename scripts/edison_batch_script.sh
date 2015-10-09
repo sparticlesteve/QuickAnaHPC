84832c69149928685d8d51b856b8a50e5225cf79
Steve Farrell <sfarrell@cern.ch>
2015-10-09 11:07:49 -0700

    update scripts to run multiple tasks per node

--- scripts/edison_batch_script.sh
+++ scripts/edison_batch_script.sh
@@ -3,14 +3,37 @@
 # Setup environment
 source edison_cvmfs_setup.sh
 
-# Configure the job
-scanDir=/global/scratch2/sd/sfarrell/xAOD
-jobDir=$SCRATCH/$PBS_JOBNAME.$PBS_JOBID
-nProofWorkers=24
+set -e
 
-# Run the QuickAnaHPC job
-runQuickAnaHPC.py --jobDir $jobDir \
-                  --task $TASKID:$NUMTASKS \
-                  --scanDir $scanDir \
-                  --driver proof --nProofWorkers $nProofWorkers \
-                  --splitSamples --opt
+# Configure the job
+scanDir=$SCRATCH/xAOD
+jobDirBase=$SCRATCH/outputs/$PBS_JOBNAME.$PBS_JOBID
+
+#otherOpts="--noSysts --maxEvents 5000"
+#otherOpts="--noSysts --maxEvents 1000 --splitSamples"
+otherOpts="--splitSamples --opt"
+
+# Calculate task range
+TASK_ID_END=$((TASK_ID_START + TASKS_PER_NODE))
+if [ $TASK_ID_END -ge $NUM_TASKS ]; then
+    TASK_ID_END=$NUM_TASKS
+fi
+
+# Loop over my assigned tasks
+for (( i = $TASK_ID_START; i < $TASK_ID_END; i++ )); do
+    echo "Starting task $i out of $NUM_TASKS"
+    jobDir=$jobDirBase.$i
+    # Run the QuickAnaHPC job in the background
+    runQuickAnaHPC.py --jobDir $jobDir \
+                      --task $i:$NUM_TASKS \
+                      --scanDir $scanDir \
+                      $otherOpts &
+    pids="$pids $!"
+done
+
+# Wait for all processes to finish
+for pid in $pids; do
+    wait $pid
+done
+
+echo "All processes have finished"
--- scripts/edison_launch_jobs.sh
+++ scripts/edison_launch_jobs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Launch N jobs and set environment variable TASKID 0-N
-N=10
-image=chos:atlas_nocondb_uc_cmssl6cbase_sqsh_v4
+numTasks=79
+tasksPerNode=24
+image=chos:atlas_nocondb_uc_cmssl6cbase_sqsh_v5
 
-for (( i=0; i < $N; i++ )); do
-    echo "Submitting job $i"
-    qsub -v TASKID=$i,NUMTASKS=$N,SHIFTER=$image edison_submit.pbs
+for (( i=0; i < $numTasks; i+=$tasksPerNode )); do
+    taskStart=$i
+    taskEnd=$((taskStart + tasksPerNode))
+    echo "Submitting tasks $taskStart-$taskEnd"
+    vars="TASK_ID_START=$taskStart,TASKS_PER_NODE=$tasksPerNode,NUM_TASKS=$numTasks,SHIFTER=$image"
+    qsub -v $vars edison_submit.pbs
 done
